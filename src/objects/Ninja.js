029868cca0266671616fc845c6e7f13d19a92b0b
Kacper Pietrzak <kacpietrzak@gmail.com>
2017-05-07 12:47:15 +0200

    Adding ninja jump

--- src/objects/Ninja.js
+++ src/objects/Ninja.js
@@ -7,12 +7,28 @@ export default class Ninja extends Phaser.Sprite{
     this.game.physics.enable( this, Phaser.Physics.ARCADE );
     this.game.world.add(this);
 
+    this.body.bounce.y = 0.2;
+    this.body.gravity.y = 300;
+
     this.mouse = this.game.input.activePointer;
 
+    this.anchor.set( 0.5, 1 );
+    this.scale.set( 0.5 );
+
     this.onDeath = new Phaser.Signal();
+
+    this.originY = this.position.y;
+
+    this.jump();
   }
   update(){
     this.body.velocity.x = this.getDirection() * VELOCITY;
+
+    if( this.position.y > this.originY && this.body.velocity.y >= 0 ){
+      console.log('stahp!');
+      this.body.allowGravity = false;
+      this.position.y = this.originY;
+    }
   }
   getDirection(){
     if( this.mouse.x + this.game.camera.x < this.position.x - NINJA_HIT_AREA_WIDTH / 2 ){
@@ -30,5 +46,8 @@ export default class Ninja extends Phaser.Sprite{
   }
   handleDeath(){
     this.onDeath.dispatch();
+  }
+  jump(){
+    this.body.velocity.y = -300;
   }
 }
